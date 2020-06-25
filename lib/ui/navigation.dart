b20cfcc126a7151c03319bbe5ecab9a895e9c586
Htin Linn Zaw <htinlinnzaw.dev@gmail.com>
2020-06-25 23:28:14 +0630

    1. add: Navigation Screen 2. change: from Counter Page to Navigation Page

---
+++ lib/ui/navigation.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class Navigation extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return NavigationState();
+  }
+}
+class NavigationState extends State<Navigation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
